00894fbbc7e7fcb3f61519eb15c02209ff6d5600
1ncen <143849645+Houdinismagic@users.noreply.github.com>
2023-10-25 12:00:20 +0200

    Delete Houdinis Magic Recovery directory

--- Houdinis Magic Recovery/include.lua
+++
@@ -1,7 +0,0 @@
-local hc = {}
-
-hc.ver = "v0.1"
-hc.support_ver = "1.67"
-hc.mpx = scripts.globals.getCharIdStr()
-
-return hc
--- Houdinis Magic Recovery/pages/Other.lua
+++
@@ -1,7 +0,0 @@
-local HC_Recovery = Submenu.add_static_submenu("Recovery", "BS_HC_Recovery")
-HC_Main:add_sub_option("Other", "BS_HC_Recovery", HC_Recovery)
-
-    HC_Recovery:add_click_option("Coming Soon", "BS_HC_recovery_Coming_Soon", function ()
-        notify.warning("Coming Soon", "Coming Soon!")
-    end)
-
